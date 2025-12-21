78bef4c778fcc731aeb7b8b2c25b361d05daa1bb
saliha <salihakamci06@gmail.com>
2025-12-21 18:58:39 +0300

    created project add backed

---
+++ backend/server.js
@@ -0,0 +1,17 @@
+const app = require("./src/app");
+const db = require("./src/models");
+require("dotenv").config();
+
+const PORT = process.env.PORT || 5000;
+
+db.sequelize.authenticate().then(() => {
+    console.log("Database connect");
+    return db.sequelize.sync();
+  }).then(() => {
+    console.log("Models synced");
+    app.listen(PORT, () => {
+      console.log(`Server on port ${PORT}`);
+    });
+  }) .catch((err) => {
+    console.error("DB connect error:", err);
+  });
