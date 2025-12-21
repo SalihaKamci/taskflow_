21cd6e23c5175d77d42f20d66d51b9b5b8b84ab4
saliha <salihakamci06@gmail.com>
2025-12-21 22:17:45 +0300

    JWTlogin , passwordHash , forcePasswordChange

--- backend/src/app.js
+++ backend/src/app.js
@@ -2,12 +2,35 @@ const express = require("express");
 const cors = require("cors");
 
 const app = express();
-
+ 
 app.use(cors());
 app.use(express.json());
+
+const authRoutes= require("./routes/authRoute");
+app.use("/api/auth",authRoutes);
+
+const { protect } = require("./middlewares/authMiddleware");
+
+app.get("/api/test/protected", protect, (req, res) => {
+  res.json({
+    message: "Protected route test",
+    user: req.user,
+  });
+});
+
+const {forcePasswordChangeCheck} = require("./middlewares/forcePasswordChange")
+app.get(
+  "/api/dashboard",
+  protect,
+  forcePasswordChangeCheck,
+  (req, res) => {
+    res.json({ message: "Dashboard erişimi başarılı" });
+  } );
 
 app.get("/health", (req, res) => {
   res.json({ status: "OK", message: "API running" });
 });
+
+
 
 module.exports = app;
---
+++ backend/src/models/user.js
@@ -0,0 +1,51 @@
+const { DataTypes } = require("sequelize");
+const sequelize = require("../config/database");
+const bcrypt = require("bcrypt");
+const User = sequelize.define(
+  "User",
+  {
+    id: {
+      type: DataTypes.INTEGER,
+      autoIncrement: true,
+      primaryKey: true,
+    },
+    fullName: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      validate: {
+        notEmpty: true,
+      },
+    },
+    email: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      unique: true,
+      validate: {
+        isEmail: true,
+      },
+    },
+    password: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    role: {
+      type: DataTypes.ENUM("admin", "employee"),
+      allowNull: false,
+      defaultValue: "employee",
+    },
+    forcePasswordChange: {
+      type: DataTypes.BOOLEAN,
+      defaultValue: false,
+    },
+  },
+  {
+    tableName: "users",
+    timestamps: true,
+  }
+);
+User.beforeCreate(async (user) => {
+  const salt = await bcrypt.genSalt(10);
+  user.password = await bcrypt.hash(user.password, salt);
+});
+
+module.exports = User;
